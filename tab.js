6d020727528ffba4248811045c6fdcedbb7167b9
ghuh243 <ghuh243@naver.com>
2026-02-01 05:08:58 +0900

    260201

--- tab.js
+++ tab.js
@@ -3,23 +3,23 @@ const tc = document.querySelectorAll(".tab-content");
 
 document.querySelector(".list").addEventListener("click", function (e) {
   
-  const 숫자 = Number(e.target.dataset.id);
+     const 숫자 = Number(e.target.dataset.id);
 
-  // 전체 초기화
-tb.forEach(function (e) {
-  e.classList.remove("orange");
+    // 전체 초기화
+    tb.forEach(function (e) {
+    e.classList.remove("orange");
+    });
+    tc.forEach(function (e) {
+    e.classList.remove("show");
+    });
+
+    // 선택된 것만 활성화
+    tb[숫자].classList.add("orange");
+    tc[숫자].classList.add("show");
 });
-tc.forEach(function (e) {
-  e.classList.remove("show");
-});
-
-  // 선택된 것만 활성화
-  tb[숫자].classList.add("orange");
-  tc[숫자].classList.add("show");
-});
 
 
-var car = '소나타';
-var carPrice = 50000;
-var carColor = 'white';
+var car2 = { name : '소나타', price : [50000, 3000, 4000] } 
 
+document.querySelector('.product').textContent = car2[name];
+document.querySelector('.product').textContent = car2.price[0];
